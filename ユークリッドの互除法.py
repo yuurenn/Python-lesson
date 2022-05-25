a699a08da9c728e94317b42fb6f6ca7619674d2c
優蓮 <94417165+yuurenn@users.noreply.github.com>
2022-05-25 23:22:47 +0900

    add append

--- ユークリッドの互除法.py
+++ ユークリッドの互除法.py
@@ -1,15 +1,23 @@
 #出力する関数作成したかった
 
+a_list=[]
+b_list=[]
+c_list=[]
+d_list=[]
 print("ユークリッドの互除法をしたい二つの数字を入力")
 a=int(input("a:"))
 b=int(input("b:"))
 i=0
 result = divmod(a, b)
-print(result[0], result[1])
+# print(result[0], result[1])
 c=int(result[0])
 d=int(result[1])
 #sequence=[a,b,c,d]
 print(str(a)+"="+str(b)+"*"+str(c)+"+"+str(d))
+a_list.append(str(a))
+b_list.append(str(b))
+c_list.append(str(c))
+d_list.append(str(d))
 # 繰り返す
 while d != 0:
     a=b
@@ -18,5 +26,12 @@ while d != 0:
     c=int(result[0])
     d=int(result[1])
     print(str(a)+"="+str(b)+"*"+str(c)+"+"+str(d))
-
+    a_list.append(str(a))
+    b_list.append(str(b))
+    c_list.append(str(c))
+    d_list.append(str(d))
+print(a_list)
+print(b_list)
+print(c_list)
+print(d_list)
 print("最大公約数",b)
